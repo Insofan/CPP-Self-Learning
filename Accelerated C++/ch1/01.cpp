4b867a8e943a3c562deed5c8e0cc99ebc38a35fa
Insofan <282132315@qq.com>
2018-04-15 20:01:58 +0800

    [add] 添加带输入框的hello

--- Accelerated C++/ch1/01.cpp
+++ Accelerated C++/ch1/01.cpp
@@ -10,7 +10,23 @@ int main() {
     std::cout << "Please enter your first name: ";
     std::string name;
     std::cin >> name;
-    std::cout << "Hello, " << name << "!" << std::endl;
+
+    const std::string greeting = "Hello, " + name + '!';
+
+    //第二行第四行
+    const std::string spaces(greeting.size(), ' ');
+    const std::string second   = "* " + spaces + " *";
+
+    //输出第一行和第五行
+    const std::string first(second.size(), '*');
+
+    std::cout << std::endl;
+    std::cout << first << std::endl;
+    std::cout << second << std::endl;
+    std::cout << "* " << greeting << " *" << std::endl;
+    std::cout << second << std::endl;
+    std::cout << first << std::endl;
+
     return 0;
 }
 
